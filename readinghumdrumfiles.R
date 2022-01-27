c762c008fc2aaa8dcc96c40cf855f82c4c4e3ef6
rkimelman <rkimelman@wustl.edu>
2022-01-27 12:41:19 -0500

    Update readinghumdrumfiles.R

--- readinghumdrumfiles.R
+++ readinghumdrumfiles.R
@@ -1,1 +1,18 @@
 library(humdrumR)
+pacCaliLoveData <- readHumdrum("2pac_CaliforniaLove.rap")
+pacHowDoUWantItData <- readHumdrum("2pac_HowDoUWantIt.rap")
+census(pacCaliLoveData)
+reference(pacCaliLoveData)
+spines(pacCaliLoveData)
+interpretations(pacCaliLoveData)
+# error - object 'exclusive' not found
+sections(pacCaliLoveData)
+# error - could not find function 'sections'
+summary(pacCaliLoveData)
+# error - code stops at interpretations
+interpretations(pacHowDoUWantItData)
+# error
+sections(pacHowDoUWantItData)
+# error
+summary(pacHowDoUWantItData)
+# error - code stops at interpretations
