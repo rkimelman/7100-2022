e98ee846d13059190dfed2f31f741916670adc09
rkimelman <rkimelman@wustl.edu>
2022-02-02 15:59:23 -0500

    Update textFunction.R

--- textFunction.R
+++ textFunction.R
@@ -1,20 +1,32 @@
-values <- c('Now', 'let', 'me', 'wel-', '-come', 'e-', '-very-', '-bo', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
+values <- c('Now', 'let', 'me', 'wel-', '-come', 'e-', '-very-', '-bo-', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
 dummyData <- data.frame(values)
 # test on dummyData, then implement into function
+save_value <- {}
 for (i in 1:nrow(dummyData)){
   splitString <- strsplit(dummyData[i, 1], "")[[1]]
-  for(j in length(splitString)){
-    print(1)
-    if(j == 1){
-      print(TRUE)
-    }
+  if(splitString[length(splitString)] == '-'){
+    save_value <- append(save_value, splitString)
   }
 }
 
 
 
-text <- function (data){
-  for (i in 1:length(data)){
+text <- function (data, iteration){
+  save_value = {}
+  for (i in range(iteration:nrow(data))){
     splitString <- strsplit(data[i, 1], "")[[1]]
+    if(splitString[length(splitString)] == '-' &&  splitString[1] == '-'){
+        save_value <- append(save_value, strsplit(data[i+1, 1], "")[[1]])
+    }
+    if(splitString[length(splitString)] == '-' && !splitString[1] == '-') {
+        save_value <- append(save_value, splitString)
+        save_value <- append(save_value, strsplit(data[i+1, 1], "")[[1]])
+    }
+    if(!splitString[length(splitString)] == '-' && splitString[1] == '-') {
+      save_value <- append(save_value, splitString)
+    }
+    else{
+      iteration = iteration + 1
+    }
   }
 }
