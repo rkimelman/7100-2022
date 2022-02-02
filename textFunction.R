9955976b128d18f30ac1fdbc2a64f9f6aeb1a48f
rkimelman <rkimelman@wustl.edu>
2022-02-02 10:00:57 -0500

    update

---
+++ textFunction.R
@@ -0,0 +1,14 @@
+values <- c('Now', 'let', 'me', 'wel-', '-come', 'e-', '-very-', '-bo', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
+dummyData <- data.frame(values)
+# test on dummyData, then implement into function
+for (i in 1:nrow(dummyData)){
+  splitString <- strsplit(dummyData[i, 1], "")[[1]]
+  print(splitString)
+}
+
+
+text <- function (data){
+  for (i in 1:length(data)){
+    splitString <- strsplit(data[i, 1], "")[[1]]
+  }
+}
