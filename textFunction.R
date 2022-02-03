7e386c1948fece6b656a855fc0680e45f22e4328
rkimelman <rkimelman@wustl.edu>
2022-02-03 16:10:01 -0500

    Update textFunction.R

--- textFunction.R
+++ textFunction.R
@@ -1,53 +1,5 @@
 values <- c('Now', 'let', 'me', 'wel-', '-come', 'e-', '-very-', '-bo-', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
 dummyData <- data.frame(values)
-# test on dummyData, then implement into function
-save_value <- c()
-save_initial_row <- 0
-save_word <- ""
-save_value <- c()
-iteration <- 1
-for (i in iteration:nrow(dummyData)){
-  splitString <- strsplit(dummyData[i, 1], "")[[1]]
-  if(splitString[length(splitString)] == '-' ||  splitString[1] == '-'){
-    if(splitString[length(splitString)] == '-' && splitString[1] != '-') {
-      save_initial_row = i
-      save_value <- append(save_value, splitString)
-      splitStringBelow <- strsplit(dummyData[i+1, 1], "")[[1]]
-      save_value <- append(save_value, splitStringBelow, "")[[1]]
-      print(save_value)
-      if(splitStringBelow[length(splitStringBelow)] != '-'){
-        # save_value <- append(save_value, splitStringBelow, "")[[1]])
-        save_value <- save_value[save_value != "-"]
-        save_word <- paste(save_value, collapse = '')
-        dummyData[save_initial_row,] <- save_word
-        dummyData[save_initial_row+1,] <- "."
-        iteration = i + 1
-        save_value <- c()
-      }
-      else{
-        dummyData[save_initial_row+1,] <- "."
-        iteration <- i+1
-      }
-    }
-    if(splitString[length(splitString)] == '-' &&  splitString[1] == '-'){
-      save_value <- append(save_value, strsplit(dummyData[i+1, 1], "")[[1]])
-      dummyData[i,] = "."
-    }
-    if(splitString[length(splitString)] != '-' && splitString[1] == '-') {
-      dummyData[i,] = "."
-      save_value <- save_value[save_value != "-"]
-      save_word <- paste(save_value, collapse = '')
-      dummyData[save_initial_row,] = save_word
-      iteration = i + 1
-      save_value <- c()
-    }
-  }
-}
-print(dummyData)
-
-values <- c('Now', 'let', 'me', 'wel-', '-come', 'e-', '-very-', '-bo-', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
-dummyData <- data.frame(values)
-
 text <- function(data, iteration){
   save_initial_row <- 0
   save_word <- ""
@@ -64,7 +16,6 @@ text <- function(data, iteration){
         save_value <- append(save_value, splitStringBelow)
         print(splitStringBelow)
         if(splitStringBelow[length(splitStringBelow)] != '-'){
-          # save_value <- append(save_value, splitStringBelow, "")[[1]])
           save_value <- save_value[save_value != "-"]
           save_word <- paste(save_value, collapse = '')
           data[save_initial_row,] <- save_word
