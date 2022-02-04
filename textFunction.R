050c317d77f9d014f70616b07097f96cfad5f797
rkimelman <rkimelman@wustl.edu>
2022-02-03 20:02:04 -0500

    Update textFunction.R

--- textFunction.R
+++ textFunction.R
@@ -43,8 +43,39 @@ text <- function(data, iteration){
     }
   return(data)
 }
-values <- c("ya'll", 'act', 'like', "you've", '-come', 'e-', '-very-', '-bo-', '-dy', 'to', 'the', 'wild', 'wild', 'west.')
+values <- c("ya'll", 'act', 'like', "you've", 'ne-', 'ver', 'seen', 'a', 'white', 'per-', 'son', 'be-', 'fore')
 dummyData <- data.frame(values)
 silbeFormat <- function(data){
-  
+  save_initials <- list()
+  print_initial <- list()
+  save_corrected <- list()
+  print_corrected <- list()
+  counter <- 0
+  for(i in 1:nrow(data)){
+    splitString <- strsplit(data[i, 1], "")[[1]]
+    splitStringNext <- strsplit(data[i+1,1], "")[[1]]
+    if(splitString[length(splitString)] == '-' && splitStringNext[1] != '-'){
+      counter <- counter + 1
+      print_initial <- append(data[i,1], data[i+1,1])
+      save_initials[counter] <- list(print_initial)
+      print_corrected <- append(data[i,1], '-')
+      print_corrected <- append(print_corrected, data[i+1, 1])
+      save_corrected[counter] <- list(print_corrected)
+    }
+  }
+  if(counter == 0){
+    return("Formatted properly.")
+  }
+  else{
+#      print("error, improperly formatted **silbe:")
+      corrections <- cat("error, improperly formatted **silbe:", save_initials[1][[1]], "should be", save_corrected[1][[1]])
+      if(counter > 1){
+        for(i in 2:counter){
+          corrections <- cat(corrections, c("and", save_initials[i][[1]], "should be", save_corrected[i][[1]]))
+        }
+      }
+  }
+#  return(corrections)
 }
+silbeFormat(dummyData)
+#print("error, improperly formatted **silbe: 'nev- er' should be 'nev- -er' and 'per- son' should be 'per- -son'")
