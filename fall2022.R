e85fdadabc515690fc919521d2f9473360fcde99
rkimelman <rkimelman@wustl.edu>
2022-09-24 13:17:25 -0400

    update code

--- fall2022.R
+++ fall2022.R
@@ -23,6 +23,8 @@ segments <- function(x, reverse = FALSE) {
   
 }
 
+
+
 mcf <- readHumdrum('.*rap')
 californiaLove <- readHumdrum('2pac_CaliforniaLove.rap')
 spinePipe(californiaLove, 2:8, 1) -> californiaLove[rev(c('Stress', 'Tone', 'Break', 'Rhyme', 'IPA', 'Lyrics', 'Hype'))]
@@ -37,45 +39,102 @@ Phrase <- californiaLove$Token %hum<% c(~list(Phrase), by ~ File ~ Phrase)
 Break <- californiaLove$Token %hum<% c(~list(Break), by ~ File ~ Phrase)
 Hype <- californiaLove$Token %hum<% c(~list(Hype), by ~ File ~ Phrase)
 
+# spinePipe(mcf, 2:8, 1) -> mcf[rev(c('Stress', 'Tone', 'Break', 'Rhyme', 'IPA', 'Lyrics', 'Hype'))]
+# mcf$Token %hum>% c(~segments(Break %in% c('3', '4','5')), by ~ File) -> mcf$Phrase
+# mcf$Token %hum<% c(~list(paste(Lyrics, collapse = ' ')), by ~ File ~ Phrase)
+# rhymeSchemes <- mcf$Token %hum<% c(~list(Rhyme), by ~ File ~ Phrase)
+# lyrics <- mcf$Token %hum<% c(~list(Lyrics), by ~ File ~ Phrase)
+# stress <- mcf$Token %hum<% c(~list(Stress), by ~ File ~ Phrase)
+# IPA <- mcf$Token %hum<% c(~list(IPA), by ~ File ~ Phrase)
+# Tone <- mcf$Token %hum<% c(~list(Tone), by ~ File ~ Phrase)
+# Phrase <- mcf$Token %hum<% c(~list(Phrase), by ~ File ~ Phrase)
+# Break <- mcf$Token %hum<% c(~list(Break), by ~ File ~ Phrase)
+# Hype <- mcf$Token %hum<% c(~list(Hype), by ~ File ~ Phrase)
 
-sigmoid <- function(x){
-  value <- 1/(1+exp(-x))
-  return(value)
+foldRecords(mcf, c('rhyme', 'stress', 'ipa'), 'recip') -> mcf
+within(mcf$Token, 
+       Rhyme <- str_extract(Rhyme, '[A-Za-z]+'),
+       soi <- SOI(duration(Token))$Onset, 
+       Rest <- Ipa == 'R', by = File) -> mcf
+subset(mcf, Stress == '1' & !Rest) -> mcf
+circleplot <- function(soi, rhymes, Bar, OTL) {
+  bars <- floor(soi)
+  newbar <- bars == soi
+  soi <- soi
+  soi <- soi / ceiling(max(soi))
+  rad <- (soi * 2 * pi) + (pi / 2)
+  x <- cos(rad)
+  y <- sin(rad)
+  plot(x, y, type = 'p', pch = 16, cex = .6, axes = FALSE, xlab = '', ylab = '')
+  # text(x, y, labels = seq_along(x))
+  text(x[newbar] * 1.1, y[newbar] * 1.1, paste0('=',bars[newbar]), xpd=T)
+  mtext(paste0("Bars ", min(Bar), ' through ', max(Bar), ' of\n', OTL), side = 3)  
+  for (r in unique(rhymes[!is.na(rhymes)])) {
+    hits <- which(rhymes == r)
+    XY <- subset(expand.grid(I = hits, J = hits), J > I)
+    with(XY, arrows(x[I], y[I], x[J], y[J], angle = 20, length = .1))
+  }
+}
+pdf('rhymeplots.pdf')
+for (i in 1:length(mcf)) {
+  print(i)
+  if (i %in% c(10, 26, 29)) next
+  with(subset(mcf[i], Bar <= 8), circleplot(soi, Rhyme,Bar,OTL))
+  with(subset(mcf[i], Bar <= 16), circleplot(soi, Rhyme,Bar,OTL))
+}
+dev.off()
+
+count <- 0
+
+for (i in 1:length(stress)){
+  for (j in 1:length(stress[i][[1]]))
+    count <- count + 1
 }
 
-rap <- rep(1, 124)
+print(count)
 
-library(dbnR)
-library(data.table)
-data(motor)
-size <- 3
-one <- c("A","A", "A")
-two <- c("D","D", "E")
-three <- c("E", "F", "F")
-four <- c("G", "G", "G")
+df <- do.call(rbind.data.frame, stress)
 
-data1<-data.frame(one)
-data2<-data.frame(two)
-data3 <- data.frame(three)
-data4 <- data.frame(four)
+capture.output(stress, file = "stress_caliLove.csv")
 
+# sigmoid <- function(x){
+#   value <- 1/(1+exp(-x))
+#   return(value)
+# }
 
-
-blacklist <- c("data1_t_0", "data2_0")
-blacklist <- rbind(blacklist, c("data1_t_2", "data2_t_1"))
-blacklist_tr <- c("data3_t_1", "data4_t_0")
-blacklist_tr <- rbind(blacklist_tr, c("data3_t_2", "data2_t_1"))
-net <- dbnR::learn_dbn_struc(data1, size, method = "dmmhc", blacklist = blacklist,
-                             blacklist_tr = blacklist_tr, restrict = "mmpc", maximize = "hc",
-                             restrict.args = list(test = "cor"),
-                             maximize.args = list(score = "bic-g", maxp = 10))
-
-f_dt_train <- fold_dt(dt_train, size)
-f_dt_val <- fold_dt(dt_val, size)
-fit <- dbnR::fit_dbn_params(net, f_dt_train, method = "mle")
-
-dbnR::plot_dynamic_network(fit)
-
-library(visNetwork)
-
-dbnR::plot_dynamic_network(fit)
+# rap <- rep(1, 124)
+# 
+# library(dbnR)
+# library(data.table)
+# data(motor)
+# size <- 3
+# one <- c("A","A", "A")
+# two <- c("D","D", "E")
+# three <- c("E", "F", "F")
+# four <- c("G", "G", "G")
+# 
+# data1<-data.frame(one)
+# data2<-data.frame(two)
+# data3 <- data.frame(three)
+# data4 <- data.frame(four)
+# 
+# 
+# 
+# blacklist <- c("data1_t_0", "data2_0")
+# blacklist <- rbind(blacklist, c("data1_t_2", "data2_t_1"))
+# blacklist_tr <- c("data3_t_1", "data4_t_0")
+# blacklist_tr <- rbind(blacklist_tr, c("data3_t_2", "data2_t_1"))
+# net <- dbnR::learn_dbn_struc(data1, size, method = "dmmhc", blacklist = blacklist,
+#                              blacklist_tr = blacklist_tr, restrict = "mmpc", maximize = "hc",
+#                              restrict.args = list(test = "cor"),
+#                              maximize.args = list(score = "bic-g", maxp = 10))
+# 
+# f_dt_train <- fold_dt(dt_train, size)
+# f_dt_val <- fold_dt(dt_val, size)
+# fit <- dbnR::fit_dbn_params(net, f_dt_train, method = "mle")
+# 
+# dbnR::plot_dynamic_network(fit)
+# 
+# library(visNetwork)
+# 
+# dbnR::plot_dynamic_network(fit)
